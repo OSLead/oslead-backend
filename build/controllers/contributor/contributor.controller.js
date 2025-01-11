9eb8ed30f336d52bbbaa7bbd5e029c4203d617c3
Saurabhchaudhary9799 <saurbhbailara@gmail.com>
2025-01-11 17:18:36 +0530

    build comman executed

--- build/controllers/contributor/contributor.controller.js
+++ build/controllers/contributor/contributor.controller.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getContributorSelf = void 0;
+exports.getAllContributors = exports.getContributorSelf = void 0;
 const contributor_model_1 = require("../../models/contributor.model");
 const response_messages_1 = require("../../utils/response_messages");
 const getContributorSelf = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -28,3 +28,29 @@ const getContributorSelf = (req, res) => __awaiter(void 0, void 0, void 0, funct
     }
 });
 exports.getContributorSelf = getContributorSelf;
+const getAllContributors = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        const contributors = yield contributor_model_1.User.find()
+            .skip(skip)
+            .limit(limit);
+        const totalContributors = yield contributor_model_1.User.countDocuments();
+        if (!contributors.length) {
+            res.status(404).send({ message: "No contributors found" });
+            return;
+        }
+        res.status(200).send({
+            total: totalContributors,
+            page,
+            limit,
+            contributors,
+        });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send({ message: response_messages_1.ERRORS_MESSAGE.ERROR_500 });
+    }
+});
+exports.getAllContributors = getAllContributors;
